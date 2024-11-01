e7944e3976fa104677eda6a0bf33577453253bf8
kwrona05 <kwrona05@gmail.com>
2024-11-01 09:31:10 +0100

    Create vite

---
+++ worldTroubles/src/components/Globe.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+const Globe = () => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [rotation, setRotation] = useState({x: 0, y: 0})
+  const [lastPosition, setLastPosition] = useState()
+};
