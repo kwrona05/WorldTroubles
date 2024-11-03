a5d63e466dcd67e1766a4666d3eb21c5b4f7300c
kwrona05 <kwrona05@gmail.com>
2024-11-03 08:27:21 +0100

    Set App.jsx as a main component

---
+++ worldTroubles/src/App.jsx
@@ -0,0 +1,10 @@
+import Globe from "./components/Globe";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="app-container">
+      <Globe />
+    </div>
+  );
+};
+export default App;
