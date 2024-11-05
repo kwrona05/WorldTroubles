da10068d354bdb03f86a7e9d4f7dfdf45f673e82
kwrona05 <kwrona05@gmail.com>
2024-11-05 11:16:59 +0100

    Add AppHeader component to App component

--- worldTroubles/src/App.jsx
+++ worldTroubles/src/App.jsx
@@ -1,8 +1,10 @@
 import Globe from "./components/Globe";
 import "./App.css";
+import AppHeader from "./components/Header";
 const App = () => {
   return (
     <div className="app-container">
+      <AppHeader />
       <Globe />
     </div>
   );
