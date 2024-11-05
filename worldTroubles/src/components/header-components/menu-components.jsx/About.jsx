c495f69cacd76cd128891f9c0f8215141ad4d437
kwrona05 <kwrona05@gmail.com>
2024-11-05 17:09:03 +0100

    Creating about dropdown component

---
+++ worldTroubles/src/components/header-components/menu-components.jsx/About.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const About = () => {
+  const AboutButton = styled.button`
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    transition: background-color 0.3s;
+  `;
+};
