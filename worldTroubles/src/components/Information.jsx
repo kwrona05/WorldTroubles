235793ae6adc2bf669c8ba39f4b2ee2a8514a229
kwrona05 <kwrona05@gmail.com>
2024-11-08 19:16:19 +0100

    Add items to list and change styles

--- worldTroubles/src/components/Information.jsx
+++ worldTroubles/src/components/Information.jsx
@@ -7,19 +7,28 @@ const InfoContainer = styled.div`
   align-items: center;
   font-size: 24px;
   padding: 20px;
-  color: ${(props) => props.theme.textColor};
+  color: #ffffff;
   transition: color 0.3s;
   width: 300px;
   word-wrap: break-word;
   overflow-wrap: break-word;
   text-align: center;
+  background-color: #1f1f1f;
+  border-radius: 10px;
+  border: 2px solid #4caf50;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 `;
 
 const Information = () => {
   const infoItems = [
-    "Konflikt na Ukrainie",
-    "Problemy z czystą wodą",
-    "Przerazające powodzie w Walencji",
+    "The Russian invasion continues!",
+    "No clean water?",
+    "Tragic floods in Valencia",
+    "No fresh air",
+    "We are losing the lungs of the Earth",
+    "Water crisis",
+    "Civil War - How much longer?",
+    "The Earth's shield is getting weaker",
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
