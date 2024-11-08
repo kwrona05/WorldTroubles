3302ca34f509a310cf4f0cab3f687fb46056f25d
kwrona05 <kwrona05@gmail.com>
2024-11-08 19:13:42 +0100

    Add details to components

--- worldTroubles/src/components/header-components/other-issues/Obesity.jsx
+++ worldTroubles/src/components/header-components/other-issues/Obesity.jsx
@@ -1,2 +1,30 @@
-const Obesity = () => {};
+const Obesity = () => {
+  return (
+    <div className="detail-container">
+      <h2>Obesity</h2>
+      <div className="description">
+        <p>
+          Obesity is a complex health condition characterized by an excess of
+          body fat, often measured using the body mass index (BMI), where a BMI
+          of 30 or higher typically indicates obesity. It results from a
+          combination of factors, including genetics, lifestyle, environment,
+          and certain health conditions. High-calorie diets, physical
+          inactivity, and metabolic factors also contribute to obesity. This
+          condition increases the risk of severe health issues like heart
+          disease, type 2 diabetes, certain cancers, and mental health
+          challenges such as depression and anxiety. Addressing obesity requires
+          a holistic approach that includes healthy dietary habits, regular
+          exercise, behavioral therapy, and sometimes medical or surgical
+          interventions. Given the high prevalence worldwide, obesity is also
+          recognized as a global public health issue, impacting healthcare
+          systems and economies significantly.
+        </p>
+        <p>
+          If you want to read more or support click
+          <a href="https://www.who.int"> here</a>
+        </p>
+      </div>
+    </div>
+  );
+};
 export default Obesity;
