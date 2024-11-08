94831812c5463843c17a1a30c610166b8694b9e9
kwrona05 <kwrona05@gmail.com>
2024-11-08 19:15:27 +0100

    Delete import and component ToggleSwitch

--- worldTroubles/src/components/Header.jsx
+++ worldTroubles/src/components/Header.jsx
@@ -1,11 +1,9 @@
-import ToggleSwitch from "./header-components/BackgroundSwitch";
 import MenuApp from "./header-components/Menu";
 const AppHeader = () => {
   return (
     <div className="app-header">
       <MenuApp />
       <h1>Beyond Border</h1>
-      <ToggleSwitch />
     </div>
   );
 };
