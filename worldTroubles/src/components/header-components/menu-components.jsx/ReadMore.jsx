562eeaae3284dd08f44421a771268d5e03dadefc
kwrona05 <kwrona05@gmail.com>
2024-11-09 21:51:05 +0100

    Update text in ReadMore component

--- worldTroubles/src/components/header-components/menu-components.jsx/ReadMore.jsx
+++ worldTroubles/src/components/header-components/menu-components.jsx/ReadMore.jsx
@@ -81,30 +81,28 @@ const ReadMore = () => {
         <DetailMenu>
           <IssuesList>
             <CountryItem>
-              <StyledLink to="/ukraine">Konflikt na ukrainie</StyledLink>
+              <StyledLink to="/ukraine">War in Ukraine</StyledLink>
             </CountryItem>
             <CountryItem>
-              <StyledLink to="/spain">
-                Przerazające powodzie w Walencji
-              </StyledLink>
+              <StyledLink to="/spain">Floods in Valencia</StyledLink>
             </CountryItem>
             <CountryItem>
-              <StyledLink to="/bangladesh">Problemy z czystą wodą</StyledLink>
+              <StyledLink to="/bangladesh">No clear water</StyledLink>
             </CountryItem>
             <CountryItem>
-              <StyledLink to="/rpa">Kryzys wody</StyledLink>
+              <StyledLink to="/rpa">Water crysis</StyledLink>
             </CountryItem>
             <CountryItem>
-              <StyledLink to="/amazonia">Amazonia</StyledLink>
+              <StyledLink to="/amazonia">Amazon deforestation</StyledLink>
             </CountryItem>
             <CountryItem>
-              <StyledLink to="/syria">Syria</StyledLink>
+              <StyledLink to="/syria">Civil war in Syria</StyledLink>
             </CountryItem>
             <CountryItem>
               <StyledLink to="/ozonehole">Ozone Hole</StyledLink>
             </CountryItem>
             <CountryItem>
-              <StyledLink to="/pekin">Pekin</StyledLink>
+              <StyledLink to="/pekin">No fresh air in Pekin</StyledLink>
             </CountryItem>
           </IssuesList>
         </DetailMenu>
