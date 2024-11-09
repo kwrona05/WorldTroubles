370ec7bb1324a3060748b35b1db72c3dc559e456
kwrona05 <kwrona05@gmail.com>
2024-11-09 21:52:05 +0100

    Create Welcome component

---
+++ worldTroubles/src/components/Welcome.jsx
@@ -0,0 +1,24 @@
+const WelcomeScreen = ({ onStart }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        gap: "20px",
+      }}
+    >
+      <h1 style={{ display: "flex", textAlign: "center" }}>
+        Welcome to the Beyond Border App
+      </h1>
+      <button
+        onClick={onStart}
+        style={{ border: "2px solid #4caf50", padding: "10px 80px" }}
+      >
+        Start
+      </button>
+    </div>
+  );
+};
+
+export default WelcomeScreen;
