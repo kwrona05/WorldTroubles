87ed24cd592e85343528781f1e1dc25f381cdea1
kwrona05 <kwrona05@gmail.com>
2024-11-09 21:49:46 +0100

    Create BackHome component

---
+++ worldTroubles/src/components/BackHome.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const BackButton = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+const BackHome = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate("/home");
+  };
+
+  return (
+    <div>
+      <BackButton onClick={handleGoBack}>Home page</BackButton>
+    </div>
+  );
+};
+export default BackHome;
