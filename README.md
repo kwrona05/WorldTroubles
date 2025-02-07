af559883de49d3c95a4f3e11ea83330326a46742
kwrona05 <kwrona05@gmail.com>
2025-02-07 22:56:15 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# World Troubles
-## The project was created to raise awareness among the digital society about the surrounding world and its challenges.
--- jest.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: "jest-environment-jsdom",
-  transform: {
-    "^.+\\.[t|j]sx?$": "babel-jest",
-  },
-  setupFilesAfterEnv: ["./jest.setup.js"],
-};
